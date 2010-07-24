89dea97a7e1fdadae686e6b3e415ba89b38459dd
Takeshi Morita <takesi.morita@gmail.com>
2010-07-25 01:06:39 +0900

    コンパイルエラーがあったのを修正． テスト用のコードはTest.scalaに分離した．

--- src/main/scala/todo_webapp/snippet/TodoWebapp.scala
+++ src/main/scala/todo_webapp/snippet/TodoWebapp.scala
@@ -51,7 +51,7 @@ class TodoWebapp {
             val user = getUserById(item.userId).get
             if (item.finishedDate != null)  {
                 css = "background-color: #cccccc;"
-                finished = item.finishedDate
+                finished = item.finishedDate.toString
                 isFinished = "true"
             } else  if (currentUser.is == user && item.deadline.getTime >= Calendar.getInstance().getTimeInMillis) {
                 css="background-color: #ffbbbb;"
@@ -475,83 +475,6 @@ class TodoWebapp {
             "userId" --> SHtml.ajaxText("", checkUserId _),
             "password" --> SHtml.ajaxText("", checkPassword _, "type" -> "password"),
             "submit" --> SHtml.submit("ログイン", auth, "id" -> "login", "disabled" -> "true"))
-    }
-
-    def ajaxTest(xhtml: NodeSeq): NodeSeq = {
-
-        var validPassword = false
-        var validUserId = false
-
-        def test2() : Any = {
-        }
-
-
-        def showMessage(id: String, msg: String) = {
-            JsCmds.SetHtml({id}, <span>{msg}</span>)
-        }
-
-        def disableLoginButton() = {
-            SetElemById("login", JsRaw("true"), "disabled")
-        }
-
-        def enableLoginButton() = {
-            SetElemById("login", JsRaw("false"), "disabled")
-        }
-
-        def checkPassword(password: String): JsCmd = {
-            val check_password_message = "check_password_msg"
-            if (password.length == 0) {
-                validPassword = false
-                showMessage(check_password_message, "パスワードが空です．") &
-                        disableLoginButton()
-            } else if (!isValidPassword(password)) {
-                validPassword = false
-                showMessage(check_password_message, "パスワードには数字またはアルファベットを入力してください．") &
-                        disableLoginButton()
-            } else {
-                validPassword = true
-                var msg = showMessage(check_password_message, "パスワード入力は正常．")
-                if (validUserId && validPassword) {
-                    msg & enableLoginButton()
-                } else {
-                    msg
-                }
-            }
-        }
-
-        def checkUserId(userId: String): JsCmd = {
-            val check_user_id_message = "check_user_id_msg"
-            if (userId.length == 0) {
-                validUserId = false
-                showMessage(check_user_id_message,"ユーザIDが空です．") &
-                        disableLoginButton()
-            } else if (!isValidUserID(userId)) {
-                validUserId = false
-                showMessage(check_user_id_message,"ユーザIDには数字またはアルファベットを入力してください．") &
-                        disableLoginButton()
-            } else {
-                validUserId = true
-                var msg = showMessage(check_user_id_message, "ユーザIDは正常．")
-                if (validUserId && validPassword) {
-                    msg & enableLoginButton()
-                } else {
-                    msg
-                }
-            }
-        }
-
-        def test(): JsCmd = {
-            println(S.param("input").openOr("none"))
-            JsIf(JsEq(ValById("input"), Str("hello")), {
-                JsCmds.SetHtml("message", <p>Hello, Ajax</p>)
-            })
-        }
-
-        bind("e", xhtml,
-            "input" --> SHtml.ajaxText("", checkUserId _),
-            "password" --> SHtml.ajaxText("", checkPassword _, "type" -> "password"),
-            "button" --> SHtml.ajaxButton("test", test _, "id" -> "login"))
-        //                 "button" --> SHtml.submit("test2", test2 _))
     }
 
 }
